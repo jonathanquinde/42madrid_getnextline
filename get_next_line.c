a9daa730e2a08951c8562dd55ac3bca7d1416cc1
jonathanquinde <jon.quindepaz@gmail.com>
2024-10-15 18:57:58 +0200

    Add files via upload
    Jejejej nose que estoy haciendo

--- get_next_line.c
+++ get_next_line.c
@@ -1,18 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jquinde- < jquinde-@student.42madrid.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 12:56:04 by jquinde-          #+#    #+#             */
+/*   Updated: 2024/10/15 12:56:04 by jquinde-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 char	*read_file(char *buffer, int fd);
 char	*join_and_free(char *buffer, char *read_buffer, int n_bytes);
-void	get_line(char *buffer, char *result);
-int		is_newline(char *read_buffer);
+void	get_line(char *buffer, char **result);
+int		is_newline(char *read_buffer, int len);
 
 char	*get_next_line(int fd)
 {
 	char static	*buffer;
 	char		*result;
 
+	result = NULL;
 	buffer = ft_calloc(1, 1);
+	if (buffer == NULL)
+		return (NULL);
 	read_file(buffer, fd);
-	get_line(buffer, result);
+	get_line(buffer, &result);
 	return (result);
 }
 
@@ -27,20 +42,18 @@ char	*read_file(char *buffer, int fd)
 	n_bytes = read(fd, read_buffer, BUFFER_SIZE);
 	while (n_bytes > 0)
 	{
-		printf("---------\nn_bytes: %d, read: |%s|:", n_bytes, read_buffer);
+		/*
+		printf("---------\nn_bytes: %d, read: \n|%s|:", n_bytes, read_buffer);
 		for (size_t i = 0; i < n_bytes; i++)
 		{
-			printf(" %d", read_buffer[i]);
-			printf("\n");
+			printf(" %d\n", read_buffer[i]);
 		}	
+		*/
 		buffer = join_and_free(buffer, read_buffer, n_bytes);
-        printf("pato1");
-		printf("-----------\n");
+		//printf("-----------\n");
 
-        printf("pato2");
-		if (is_newline(read_buffer))
+		if (is_newline(read_buffer, n_bytes))
 		{
-            printf("Hey esto funciona");
 			free(read_buffer);
 			return (buffer);
 		}
@@ -70,31 +83,35 @@ char	*join_and_free(char *old_buffer, char *read_buffer, int n_bytes)
 		new_buffer[i] = read_buffer[i - buffer_len];
 		i++;
 	}
-    printf("Old Buffer: |%s|, size: %zu\n", old_buffer, buffer_len);
+    //printf("Old Buffer: \n|%s|\nsize: %zu\n", old_buffer, buffer_len);
     new_buffer[i] = 0;
-    printf("New Buffer: |%s|, size: %zu\n", new_buffer, buffer_len + n_bytes);
-	free(old_buffer);
+	free (old_buffer);
+    //printf("New Buffer: \n|%s|\nsize: %zu\n", new_buffer, buffer_len + n_bytes);
 	return (new_buffer);
 }
 
-void	get_line(char *buffer, char *result)
+void	get_line(char *buffer, char **result)
 {
-	result = malloc(ft_strlen(buffer) + 1);
-	ft_strlcpy(result, buffer, ft_strlen(buffer) + 1);
+	size_t len;
 
+	len = ft_strlen(buffer);
+	*result = malloc(len + 1);
+	if (*result == NULL)
+		return ;
+	ft_strlcpy(*result, buffer, len + 1);
 	free(buffer);
 }
 
-int	is_newline(char *read_buffer)
+int	is_newline(char *read_buffer, int len)
 {
-	while (*read_buffer)
+	size_t	i;
+
+	i = 0;
+	while (i < len)
 	{
-		if (*read_buffer == '\n')
-        {
-            printf("pato3");
+		if (read_buffer[i] == '\n')
 			return (1);
-        }
-		read_buffer++;
+		i++;
 	}
 	return (0);
 }
